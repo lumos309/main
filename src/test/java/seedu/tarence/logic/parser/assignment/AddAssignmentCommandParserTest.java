86fe0c5814d3a19acb08ff9bb45986094077fc2a
lumos309 <tan.yugin1@gmail.com>
2019-10-30 02:22:39 +0800

    Add tests for Event commands

--- src/test/java/seedu/tarence/logic/parser/assignment/AddAssignmentCommandParserTest.java
+++ src/test/java/seedu/tarence/logic/parser/assignment/AddAssignmentCommandParserTest.java
@@ -1,15 +1,26 @@
-package seedu.tarence.logic.parser;
+package seedu.tarence.logic.parser.assignment;
 
 import static seedu.tarence.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.tarence.logic.parser.CliSyntax.PREFIX_END_DATE;
-import static seedu.tarence.logic.parser.CliSyntax.PREFIX_INDEX;
-import static seedu.tarence.logic.parser.CliSyntax.PREFIX_MODULE;
-import static seedu.tarence.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.tarence.logic.parser.CliSyntax.PREFIX_SCORE;
-import static seedu.tarence.logic.parser.CliSyntax.PREFIX_START_DATE;
-import static seedu.tarence.logic.parser.CliSyntax.PREFIX_TUTORIAL_NAME;
 import static seedu.tarence.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.tarence.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.tarence.logic.parser.ParserTestUtils.ASSIGN_NAME_DESC;
+import static seedu.tarence.logic.parser.ParserTestUtils.END_DATE_DESC;
+import static seedu.tarence.logic.parser.ParserTestUtils.INDEX_DESC;
+import static seedu.tarence.logic.parser.ParserTestUtils.INVALID_INDEX_DESC;
+import static seedu.tarence.logic.parser.ParserTestUtils.INVALID_SCORE_DESC_NEGATIVE;
+import static seedu.tarence.logic.parser.ParserTestUtils.INVALID_SCORE_DESC_NON_NUMBER;
+import static seedu.tarence.logic.parser.ParserTestUtils.INVALID_START_DATE_DESC;
+import static seedu.tarence.logic.parser.ParserTestUtils.MOD_CODE_DESC;
+import static seedu.tarence.logic.parser.ParserTestUtils.SCORE_DESC;
+import static seedu.tarence.logic.parser.ParserTestUtils.START_DATE_DESC;
+import static seedu.tarence.logic.parser.ParserTestUtils.TUT_NAME_DESC;
+import static seedu.tarence.logic.parser.ParserTestUtils.VALID_ASSIGN_NAME;
+import static seedu.tarence.logic.parser.ParserTestUtils.VALID_END_DATE;
+import static seedu.tarence.logic.parser.ParserTestUtils.VALID_INDEX;
+import static seedu.tarence.logic.parser.ParserTestUtils.VALID_MOD_CODE;
+import static seedu.tarence.logic.parser.ParserTestUtils.VALID_SCORE;
+import static seedu.tarence.logic.parser.ParserTestUtils.VALID_START_DATE;
+import static seedu.tarence.logic.parser.ParserTestUtils.VALID_TUT_NAME;
 import static seedu.tarence.logic.parser.ParserUtil.MESSAGE_INVALID_INDEX;
 
 import java.text.ParseException;
@@ -19,36 +30,12 @@ import org.junit.jupiter.api.Test;
 
 import seedu.tarence.commons.core.index.Index;
 import seedu.tarence.logic.commands.assignment.AddAssignmentCommand;
-import seedu.tarence.logic.parser.assignment.AddAssignmentCommandParser;
 import seedu.tarence.model.module.ModCode;
 import seedu.tarence.model.tutorial.Assignment;
 import seedu.tarence.model.tutorial.TutName;
 import seedu.tarence.model.tutorial.Tutorial;
 
 public class AddAssignmentCommandParserTest {
-
-    public static final String VALID_ASSIGN_NAME = "Assignment 1";
-    public static final String VALID_END_DATE = "10-10-11 1000";
-    public static final int VALID_INDEX = 1;
-    public static final String VALID_MOD_CODE = "ZA9876X";
-    public static final int VALID_SCORE = 10;
-    public static final String VALID_START_DATE = "10-10-10 1000";
-    public static final String VALID_TUT_NAME = "Studio 99";
-
-    public static final int INVALID_INDEX = 0;
-
-    public static final String ASSIGN_NAME_DESC = " " + PREFIX_NAME + VALID_ASSIGN_NAME;
-    public static final String END_DATE_DESC = " " + PREFIX_END_DATE + VALID_END_DATE;
-    public static final String INDEX_DESC = " " + PREFIX_INDEX + VALID_INDEX;
-    public static final String MOD_CODE_DESC = " " + PREFIX_MODULE + VALID_MOD_CODE;
-    public static final String SCORE_DESC = " " + PREFIX_SCORE + VALID_SCORE;
-    public static final String START_DATE_DESC = " " + PREFIX_START_DATE + VALID_START_DATE;
-    public static final String TUT_NAME_DESC = " " + PREFIX_TUTORIAL_NAME + VALID_TUT_NAME;
-
-    public static final String INVALID_INDEX_DESC = " " + PREFIX_INDEX + INVALID_INDEX;
-    public static final String INVALID_SCORE_DESC_NEGATIVE = " " + PREFIX_SCORE + "-1";
-    public static final String INVALID_SCORE_DESC_NON_NUMBER = " " + PREFIX_SCORE + "abcde";
-    public static final String INVALID_START_DATE_DESC = " " + PREFIX_START_DATE + "some random string";
 
     private AddAssignmentCommandParser parser = new AddAssignmentCommandParser();
 
